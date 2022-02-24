f984ce1b37848a2258fbf422b866e5de88656a9e
manbil03 <bill@atomix.com.au>
2022-02-24 21:46:49 +1100

    feat/create balancesOverTime table for the previous 120 blocks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import AppLayout from '@app/ui/layouts/App'
 import { PageContent } from '@app/ui/components/PageContent'
 import { PageWrapper } from '@app/ui/components/PageWrapper'
 import pxToRem from '@app/ui/utils/pxToRem';
 import styled from 'styled-components';
-import { getManyBalances, getBalancesFromTime } from '@app/ui/utils/web3'
+import { getBalancesOverTime } from '@app/ui/utils/web3'
 
-type IndexPageProps = {
-
+type wallet = {
+  [key: string]: number
 }
+
 
 const top5Wallets = [
  '0x00000000219ab540356cbb839cbe05303d7705fa',
@@ -18,10 +19,80 @@ const top5Wallets = [
  '0x011b6e24ffb0b5f5fcc564cf4183c5bbbc96d515'
 ]
 
-const top5Balances = await getManyBalances(top5Wallets)
-console.log('index', await getBalancesFromTime())
+export const IndexPage: FC = () => {
 
-export const IndexPage: FC<IndexPageProps> = ({}) => {
+  const [tableData, setTableData] = useState<string[][]>()
+
+  const initTableData = async () => {
+    const [first, second, third, fourth, fifth] = await Promise.all(
+      top5Wallets.map(async (wallet: string) => {
+        return await getBalancesOverTime(wallet)
+      })
+    )
+
+    return [first, second, third, fourth, fifth ]
+  }
+  initTableData().then(res => {
+    setTableData(res)
+  })
+
+  // Infura free plan rate limit hit (no surprise)
+  // TODO: implement caching
+
+  const data = ([
+    [
+      '9579042.00',
+      '9579042.00',
+      '9579042.00',
+      '9579042.00',
+      '9579042.00',
+      '9579042.00',
+      '9579042.00',
+      '9579042.00'
+    ],
+    [
+      '2003504.57',
+      '2003504.57',
+      '2003504.57',
+      '2003504.57',
+      '2003504.57',
+      '2003504.57',
+      '2003504.57',
+      '2003504.57'
+    ],
+    [
+      '994999.491',
+      '994834.492',
+      '994829.492',
+      '994824.492',
+      '994824.492',
+      '994824.492',
+      '995024.492',
+      '994999.491'
+    ],
+    [
+      '575220.379',
+      '575250.564',
+      '575267.626',
+      '575363.856',
+      '575323.296',
+      '575220.379',
+      '575220.379',
+      '575220.379'
+    ],
+    [
+      '569354.336',
+      '569337.925',
+      '569308.775',
+      '569309.518',
+      '569312.001',
+      '569356.536',
+      '569353.586',
+      '569354.336'
+    ]
+  ])
+
+
   return (
     <AppLayout>
       <PageContent>
@@ -29,11 +100,35 @@ export const IndexPage: FC<IndexPageProps> = ({}) => {
           <h1>DORP</h1>
           <hr />
           <BalanceWrapper>
-            {top5Balances?.map((balance: string, i: number) => (
-              <BalanceRow key={i + 1}>
-                <h2>Wallet No. {i + 1}: {balance}</h2>
-              </BalanceRow>
-            ))}
+            <table>
+              <thead>
+                <tr>
+                  <th>Current</th>
+                  <th>120</th>
+                  <th>80</th>
+                  <th>40</th>
+                  <th>20</th>
+                  <th>10</th>
+                  <th>5</th>
+                  <th>1</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data ? data?.map((period: any, i: number) => (
+                  <tr key={i}>
+                    <td>{period[0]}</td>
+                    <td>{period[1]}</td>
+                    <td>{period[2]}</td>
+                    <td>{period[3]}</td>
+                    <td>{period[4]}</td>
+                    <td>{period[5]}</td>
+                    <td>{period[6]}</td>
+                    <td>{period[7]}</td>
+                  </tr>
+                ))
+              : ''}
+              </tbody>
+            </table>
           </BalanceWrapper>
         </PageWrapper>
       </PageContent>
--- src/ui/utils/web3/index.ts
+++ src/ui/utils/web3/index.ts
@@ -1,35 +1,36 @@
 
 import Web3 from 'web3'
-// @ts-ignore
-import EthDater from 'ethereum-block-by-date'
-import moment from 'moment'
 
 const rpcURL = 'https://mainnet.infura.io/v3/' + process.env.NEXT_PUBLIC_INFURA_API_KEY
 const web3 = new Web3(rpcURL)
 
-export const getBalance = async (address: string) => {
-  const bal = web3.eth.getBalance(address)
+export const latestBlock = await web3.eth.getBlockNumber()
+export const blockTime = [
+	latestBlock,
+	latestBlock - 120,
+	latestBlock - 80,
+	latestBlock - 40, 
+	latestBlock - 20,
+	latestBlock - 10,
+	latestBlock - 5,
+	latestBlock - 1
+]
+
+export const getBalance = async (address: string, reqBlock?: number) => {
+	let bal
+	if (reqBlock) {
+		bal = await web3.eth.getBalance(address, reqBlock)
+	} else {
+		bal = web3.eth.getBalance(address)
+	}
   return web3.utils.fromWei(await bal, 'ether').slice(0, 10)
 }
 
-export const getManyBalances = async (wallets: string[]) => {
-  return Promise.all(wallets.map(async (wallet: string) => {
-    return getBalance(wallet);
-  }));
-}
+export const getBalancesOverTime = async (wallet: string) => {
+	const [a, b, c, d, e, f, g, h] = await Promise.all(
+		blockTime.map(async (time: number) => {
+			return await getBalance(wallet, time)
+	}))
 
-export const getBalancesFromTime = async (wallets: string[]) => {
-
-	const dater = new EthDater(web3)
-
-	//Get block form 6 months ago
-	let block = await dater.getDate(moment().subtract(6, 'months'),true)
-	console.log('blok', block.block)
-
-	//Can only get the most previous 128 blocks with free infura
-
-	// Pivot app to show change in wallet amount in the most previous 128 blocks
-	// Post on twitter when a trend is occuring in the most previous 128 blocks
-	console.log('bal', await web3.eth.getBalance('0x00000000219ab540356cbb839cbe05303d7705fa', 13060497))
-
+	return [a, b, c, d, e, f, g, h]
 }
