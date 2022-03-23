ed0554b7fef7d7a5f5d719a1ae4d7964c58a48c1
bill-mansfield <manbil03@gmail.com>
2022-03-24 08:36:53 +1100

    feat/setup twitter post endpoint

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,22 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  let response = await fetch('https://api.twitter.com/1.1/statuses/update.json?status=hello', {
+		headers: {
+			"authorization": "OAuth",
+			"oauth_consumer_key": "",
+      "oauth_nonce": "",
+      "oauth_signature": "",
+      "oauth_signature_method": "HMAC-SHA1",
+      "oauth_timestamp": "generated_timestamp",
+      "oauth_token": "",
+      "oauth_version": "1.0"
+		},
+	})
+	if (!response.ok) {
+		const message = `An error has occured: ${response.status}`
+		throw new Error(message)
+	}
+	return response.json()
+
+  // res.status(200).json({ name: 'John Doe' })
+
 }
